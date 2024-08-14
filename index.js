45360697959edd79e819d66ab18efda7f61f6865
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-08-14 11:17:36 +0530

    Added Chat to Database,retrieved past messages

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http=require('http');
 const socketio = require("socket.io");
 const connect=require('./config/database-config');
 
+const chat=require('./models/chat');
+
 const app=express();
 const server = http.createServer(app);
 const io =  socketio(server);
@@ -16,8 +18,13 @@ io.on('connection', (socket) => {
         });
     });
 
-    socket.on('msg_send',(data)=>{
+    socket.on('msg_send',async (data)=>{
         console.log(data);
+        const newChat=await chat.create({
+            content:data.msg,
+            user:data.username,
+            roomId:data.roomid
+        });
         io.to(data.roomid).emit('msg_rcvd',data);
         //socket.emit('msg_rcvd',data);
         //socket.broadcast.emit('msg_rcvd',data);
@@ -27,15 +34,19 @@ io.on('connection', (socket) => {
 
 app.set('view engine','ejs');
 app.use('/',express.static(__dirname+'/public'));
-app.get('/chat/:roomid',(req,res)=>{
+app.get('/chat/:roomid',async (req,res)=>{
+    const chats=await chat.find({
+        roomId:req.params.roomid
+    }).select('content user');
     res.render('index',{
         name:'Srinu',
-        id:req.params.roomid
+        id:req.params.roomid,
+        chats:chats
     });
 });
 
 server.listen(3000,async ()=>{
     console.log(`Server started`);
-    //await connect();
+    await connect();
     console.log("MongoDB connected");
 });
